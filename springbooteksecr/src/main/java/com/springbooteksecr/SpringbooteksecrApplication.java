14e3c4c4ad5d7dec3cb2762b7e2fbdac573077ea
upadhyay-rajesh <upadhyay.rajesh@rediffmail.com>
2024-07-09 11:10:54 -0400

    first commit

---
+++ springbooteksecr/src/main/java/com/springbooteksecr/SpringbooteksecrApplication.java
@@ -0,0 +1,13 @@
+package com.springbooteksecr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbooteksecrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbooteksecrApplication.class, args);
+	}
+
+}
---
+++ springbooteksecr/src/test/java/com/springbooteksecr/SpringbooteksecrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbooteksecr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbooteksecrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
